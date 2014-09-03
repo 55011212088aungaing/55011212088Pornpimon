adeaa59dc28164cd39b0a1ca52e24781fc966046
55011212088aungaing <iMac@msu>
2014-09-03 15:37:10 +0700

    new

--- zoo.playground/section-1.swift
+++ zoo.playground/section-1.swift
@@ -2,16 +2,23 @@ class Zoo{
     let name : String
     let food : String
     
+    var Newname:[String] = ["Birds","Elephant","Tiger"]
+    var Newfood:[String] = ["Worm","Cane","Beef"]
+    
+    
     init(name:String, food:String){
         self.name = name
         self.food = food
-        
-    }
-    func AddName() ->String{
-        var name:Dictionary<String,Int> = ["Birds":1,"Elephant":2,"Tiger":3]
-        var food:Dictionary<String,Int> = ["Worm":1,"Cane":2,"Beef":3]
-        
-        return " "
     }
     
+    func  Zooname (){
+        Newname += name
+        Newfood += food
+    }
+    
+    
 }
+
+let test = Zoo(name:"monky",food:"Banana")
+test.Zooname()
+
